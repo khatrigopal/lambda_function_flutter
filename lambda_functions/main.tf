61fbf92fe8b7fd7e39305d3425d2239ca92337a1
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-15 23:41:11 +0530

    Update main.tf

--- lambda_functions/main.tf
+++ lambda_functions/main.tf
@@ -6,7 +6,7 @@ module "lambda_function_1" {
   source = "./modules/lambda_function"
 
   function_name = "lambda_function_1"
-  role_arn = "arn:aws:iam::123456789012:role/lambda-role"
+  
   handler = "lambda_function_1.handler"
   runtime = "python3.8"
   memory_size = 256
