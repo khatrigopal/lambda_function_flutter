1814033c42091991d1d45242977d98006af1607f
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-15 23:28:25 +0530

    Create variables.tf

---
+++ modules/lambda_function/variables.tf
@@ -0,0 +1,30 @@
+variable "function_name" {
+  description = "The name of the AWS Lambda function."
+}
+
+variable "role_arn" {
+  description = "The ARN of the IAM role for the AWS Lambda function."
+}
+
+variable "handler" {
+  description = "The name of the Python function that AWS Lambda calls to start execution."
+}
+
+variable "runtime" {
+  description = "The runtime environment for the AWS Lambda function."
+  default = "python3.8"
+}
+
+variable "memory_size" {
+  description = "The amount of memory that AWS Lambda allocates to the function."
+  default = 128
+}
+
+variable "timeout" {
+  description = "The amount of time that AWS Lambda allows a function to run before stopping it."
+  default = 3
+}
+
+variable "filename" {
+  description = "The filename of the Python file for the AWS Lambda function."
+}
