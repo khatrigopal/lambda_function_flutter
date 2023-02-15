076cfc9f28dfeb84f440d0164c58b46f5e27a176
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-15 23:39:41 +0530

    Update main.tf

--- modules/lambda_function/main.tf
+++ modules/lambda_function/main.tf
@@ -1,3 +1,48 @@
+resource "aws_iam_role" "lambda_role" {
+name   = "Spacelift_Test_Lambda_Function_Role"
+assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "lambda.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+ 
+ name         = "aws_iam_policy_for_terraform_aws_lambda_role"
+ path         = "/"
+ description  = "AWS IAM Policy for managing aws lambda role"
+ policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "logs:CreateLogGroup",
+       "logs:CreateLogStream",
+       "logs:PutLogEvents"
+     ],
+     "Resource": "arn:aws:logs:*:*:*",
+     "Effect": "Allow"
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+ role        = aws_iam_role.lambda_role.name
+ policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+
 data "archive_file" "function" {
   type = "zip"
   source_file = var.filename
@@ -5,7 +50,7 @@ data "archive_file" "function" {
 
 resource "aws_lambda_function" "this" {
   function_name = var.function_name
-  role = var.role_arn
+  role = aws_iam_role.lambda_role.arn
   handler = var.handler
   runtime = var.runtime
   memory_size = var.memory_size
