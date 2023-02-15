4cd45a24ca0db8a43332cf50ff82ce02d1eb251a
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-16 00:02:03 +0530

    Update variables.tf

--- lambda_functions/variables.tf
+++ lambda_functions/variables.tf
@@ -1,11 +1,13 @@
 variable "function_name" {
   description = "The name of the AWS Lambda function."
+  default = ""
 }
 
 
 
 variable "handler" {
   description = "The name of the Python function that AWS Lambda calls to start execution."
+  default = ""
 }
 
 variable "runtime" {
