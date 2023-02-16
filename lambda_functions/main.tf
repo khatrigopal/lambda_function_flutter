d2d5d3836ea2874d21b405b920837860ec7d6579
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-16 11:10:15 +0530

    Update main.tf

--- lambda_functions/main.tf
+++ lambda_functions/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+source_code_path = ../lambda_functions/lambda_function_1.py
 module "lambda_function_1" {
   filename = var.filename
   source = "../modules/lambda_function"
