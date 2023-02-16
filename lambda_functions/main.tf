4363b99ddb8a8f923afbaae71c810f1d505d100f
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-16 10:58:16 +0530

    Update main.tf

--- lambda_functions/main.tf
+++ lambda_functions/main.tf
@@ -4,14 +4,9 @@ provider "aws" {
 
 module "lambda_function_1" {
   source = "../modules/lambda_function"
-  
-
   function_name = "lambda_function_1"
-  
   handler = "lambda_function_1.handler"
   runtime = "python3.8"
   memory_size = 256
   timeout = 10
-
-  filename = "lambda_functions/lambda_function_1.zip"
-}
+ }
